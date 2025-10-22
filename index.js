f46493de33c295b8bd9e4206a8c50e4eaacad0ba
sebastianthomas94 <53168297+sebastianthomas94@users.noreply.github.com>
2025-10-22 10:29:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 1337;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.text({ type: ["text/plain", "text/html", "*/*"], limit: "10mb" })
+);
+
+// create a rout that proxies every request to google.com
+
+const PROXY_URL = process.env.PROXY_URL;
+
+if (!PROXY_URL) {
+  console.error("Error: PROXY_URL is not defined in environment variables.");
+  process.exit(1);
+}
+
+app.use(async (req, res) => {
+  try {
+    const url = `${PROXY_URL}${req.originalUrl}`;
+    console.log(`Proxying request to: ${url}`);
+
+    const response = await fetch(url, {
+      method: req.method,
+      headers: { ...req.headers, host: new URL(PROXY_URL).host },
+      body:
+        req.method !== "GET" && req.method !== "HEAD" ? req.body : undefined,
+    });
+
+    const data = await response.text();
+    res.status(response.status).send(data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
